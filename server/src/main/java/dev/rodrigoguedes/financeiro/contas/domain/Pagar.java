82828d44e8bcf6bd7883ec2a69f107eda53f8b7b
Rodrigo Guedes de Souza <rodrigo@guedesdesouza.com.br>
2023-04-28 01:06:51 -0300

    Adicionando algumas configurações para a persistência de dados

--- server/src/main/java/dev/rodrigoguedes/financeiro/contas/domain/Pagar.java
+++ server/src/main/java/dev/rodrigoguedes/financeiro/contas/domain/Pagar.java
@@ -2,15 +2,63 @@ package dev.rodrigoguedes.financeiro.contas.domain;
 
 import java.util.UUID;
 
-import dev.rodrigoguedes.core.domain.CoreEntity;
+import javax.money.MonetaryAmount;
 
+import org.hibernate.annotations.CompositeType;
+
+import dev.rodrigoguedes.core.domain.CoreEntity;
+import io.hypersistence.utils.hibernate.type.money.MonetaryAmountType;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.ToString;
+
+@Entity
+@Table(name = "fin_pagar")
+@ToString
 public class Pagar extends CoreEntity<UUID> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
+
+    @Version
+    @Column(name = "version")
+    Integer version;
+
+    @Column(name = "descricao", length = 100)
+    private String descricao;
+
+    @AttributeOverride(name = "amount", column = @Column(name = "valor"))
+    @AttributeOverride(name = "currency", column = @Column(name = "moeda"))
+    @CompositeType(MonetaryAmountType.class)
+    private MonetaryAmount valor;
 
     @Override
     public UUID getId() {
         return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public MonetaryAmount getValor() {
+        return valor;
+    }
+
+    public void setValor(MonetaryAmount valor) {
+        this.valor = valor;
     }
 
 }
--- server/src/main/java/dev/rodrigoguedes/financeiro/contas/web/resource/PagarResource.java
+++ server/src/main/java/dev/rodrigoguedes/financeiro/contas/web/resource/PagarResource.java
@@ -1,13 +1,33 @@
 package dev.rodrigoguedes.financeiro.contas.web.resource;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import dev.rodrigoguedes.financeiro.contas.domain.Pagar;
 import dev.rodrigoguedes.financeiro.contas.service.PagarService;
-import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 
-@Resource
+@RestController
+@RequestMapping("/financeiro/pagar")
 public class PagarResource {
 
     @Autowired
     private PagarService pagarService;
+
+    @GetMapping
+    private ResponseEntity<Page<Pagar>> findAll(
+            @RequestParam(value = "search", required = false) Optional<String> search,
+            Pageable pageable, HttpServletRequest request) {
+
+        return ResponseEntity.ok(pagarService.findAll(pageable));
+    }
+
 }
