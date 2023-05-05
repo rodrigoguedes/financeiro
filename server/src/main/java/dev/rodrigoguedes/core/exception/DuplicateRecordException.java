7e8e8015b6120507693fa99c608cdc89fb978d59
Rodrigo Guedes de Souza <rodrigo@guedesdesouza.com.br>
2023-05-05 01:13:30 -0300

    Cria exception de negócio que trata informações duplicadas

---
+++ server/src/main/java/dev/rodrigoguedes/core/exception/DuplicateRecordException.java
@@ -0,0 +1,9 @@
+package dev.rodrigoguedes.core.exception;
+
+public class DuplicateRecordException extends RuntimeException {
+
+    public DuplicateRecordException(String message) {
+        super(message);
+    }
+
+}
