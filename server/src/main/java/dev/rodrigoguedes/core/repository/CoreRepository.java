28697e9e6e56d9044042ec37811c75760a5fc52d
Rodrigo Guedes de Souza <rodrigo@guedesdesouza.com.br>
2023-05-05 01:10:16 -0300

    Refatoração do CoreRepository

--- server/src/main/java/dev/rodrigoguedes/core/repository/CoreRepository.java
+++ server/src/main/java/dev/rodrigoguedes/core/repository/CoreRepository.java
@@ -5,17 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CoreRepository<T, K extends Serializable>
-        extends org.springframework.data.repository.Repository<T, K>, JpaSpecificationExecutor<T> {
+public interface CoreRepository<T, K extends Serializable> extends JpaRepository<T, K> {
 
     Optional<T> findOneOptionalById(K id);
-
-    T findOneById(K id);
-
-    Iterable<T> findAll(Sort sort);
 
     Page<T> findAll(Pageable pageable);
 
